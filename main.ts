8d83a86c7e18bbc9cd424b7e6c1901d8e981566c
greenybeeny <92817241+greenybeeny@users.noreply.github.com>
2021-11-10 13:43:26 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,9 @@
 basic.forever(function () {
+    // if the light level is above 128 then show string
     if (input.lightLevel() > 128) {
         basic.showString("Wakey Wakey")
     } else {
+        // show image of person sleeping if light level is below 128 
         images.createBigImage(`
             . . . . . . # # . .
             . . . . . . . # # .
